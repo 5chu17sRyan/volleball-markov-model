ecb111ecdb368cb2fda526567f7b41969d78f5fd
shanti843 <silver1@kenyon.edu>
2021-04-10 13:29:28 -1000

    includes k-fold cross validation

--- multiple_regression.R
+++ multiple_regression.R
@@ -5,15 +5,18 @@ library(Hmisc)
 library(ggpubr)
 library(lmtest)
 library(glmnet)
-
-#Multiple Linear Regression
+library(caret)
+library(randomForest)
+#MULTIPLE LINEAR REGRESSION
 multi_data <- read.csv("C:/Users/18083/Desktop/202110/Sports Analytics/Baseball/strikeout_many_variables.csv")
 multi_data$percent_strikeout <- 100*(multi_data$p_strikeout/multi_data$p_total_pa)
 multi_data <- multi_data %>%
   select(c(percent_strikeout, ff_avg_speed, ff_avg_spin, ff_avg_break_x, ff_avg_break_z))
-
-multi_model <- glm(percent_strikeout~ ff_avg_speed + ff_avg_spin + ff_avg_break_x +ff_avg_break_z, data=multi_data )
+multi_data <- multi_data[complete.cases(multi_data),]
+multi_model <- lm(percent_strikeout~ ff_avg_speed + ff_avg_spin + ff_avg_break_x +ff_avg_break_z, data=multi_data )
 summary(multi_model)
+#Adjusted R-squared:  0.1877 
+#Residual Standard Error: 6.304 on 442 degrees of freedom
 
 #multivariate normality
 plot(multi_model, 2) #looks ok
@@ -32,9 +35,9 @@ mydata.p #got some issues
 #RIDGE REGESSION
 #https://www.statology.org/ridge-regression-in-r/
 #make the model
-multi_complete_data <- multi_data[complete.cases(multi_data),]
-y <- multi_complete_data$percent_strikeout
-x <- data.matrix(multi_complete_data[,c('ff_avg_speed', 'ff_avg_spin', 'ff_avg_break_x', 'ff_avg_break_z')])
+
+y <- multi_data$percent_strikeout
+x <- data.matrix(multi_data[,c('ff_avg_speed', 'ff_avg_spin', 'ff_avg_break_x', 'ff_avg_break_z')])
 ridge_model <- glmnet(x, y, alpha = 0)
 summary(ridge_model)
 
@@ -51,6 +54,7 @@ plot(cv_model)
 #look at the best model
 best_model <- glmnet(x, y, alpha = 0, lambda = best_lambda)
 coef(best_model)
+best_model #the percent (of null) deviance explained (%dev) = 19.32
 
 #produce Ridge trace plot to visualize how the coefficient estimates changed as a result of increasing lambda
 plot(model, xvar = "lambda")
@@ -73,4 +77,22 @@ plot(cv_model2)
 #look at the best model
 best_model2 <- glmnet(x, y, alpha = 1, lambda = best_lambda)
 coef(best_model2)
+best_model2
+#the percent (of null) deviance explained (%dev) = 15.32
+
+#K FOLD CROSS VALIDATION
+#https://www.statology.org/k-fold-cross-validation-in-r/
+
+#specify the cross-validation method
+ctrl <- trainControl(method = "cv", number = 5)
+
+#fit a regression model and use k-fold CV to evaluate performance
+lassoo_cv <- train(x, y, alpha = 1, lambda = best_lambda, trControl = ctrl)
+ridge_cv <- train(x, y, alpha = 0, lambda = best_lambda, trControl = ctrl)
+linear_cv <- train(percent_strikeout~., data=multi_data, method="lm", trControl = ctrl)
+
+#view summary of k-fold CV               
+print(lassoo_cv) # 2    6.579732  0.1489319  5.178836
+print(ridge_cv) # 2     6.552741  0.1488891  5.165473 
+print(linear_cv) #      6.330036  0.2051039  4.936704
 
