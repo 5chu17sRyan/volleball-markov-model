700951418ad7fc6e38a93871276e2753d066eb87
shanti843 <silver1@kenyon.edu>
2021-04-09 14:58:31 -1000

    Added ridge regression

--- multiple_regression.R
+++ multiple_regression.R
@@ -2,53 +2,27 @@
 require(tidyverse)
 require(dplyr)
 library(Hmisc)
-
-#Ryan did this
-# LINEAR REGRESSION
-# #loading in the data
-# data <- read.csv("C:/Users/18083/Desktop/202110/Sports Analytics/Baseball/strikeouts_pitchspin.csv")
-# 
-# #cleaning up column names
-# data <- data %>%
-#   select(-c(player_id, X))
-# 
-# names <- c("last_name", 'first_name', 'year', 'batters_facing', 'strikeouts', 'average_spin')
-# colnames(data) <- names
-# 
-# data$percent_strikeout <- data$strikeouts/data$batters_facing
-# data$percent_strikeout <- data$percent_strikeout*100
-# 
-# #write out file
-# #write.csv(data, "C:/Users/18083/Desktop/202110/Sports Analytics/Baseball/strikeout_data.csv")
-# 
-# #LEAST SQUARES REGRESSION
-# #check for linear relationship
-# plot(data$percent_strikeout, data$average_spin) #kind of
-# 
-# #make model
-# model <- glm(percent_strikeout~., data=data)
-# 
-# #check for homoscedasticity
-# plot(model, 1) #fat in the middle
-# 
-# #check for normality
-# plot(model, 2) 
+library(ggpubr)
+library(lmtest)
+library(glmnet)
 
 #Multiple Linear Regression
 multi_data <- read.csv("C:/Users/18083/Desktop/202110/Sports Analytics/Baseball/strikeout_many_variables.csv")
-multy_data <- multi_data %>%
-  select(c(percent_strikeout, ff_avg_speed, ff_avg_spin, ff_avg_break_x, ff_avg_break_z))
 multi_data$percent_strikeout <- 100*(multi_data$p_strikeout/multi_data$p_total_pa)
-head(multi_data)
+multi_data <- multi_data %>%
+  select(c(percent_strikeout, ff_avg_speed, ff_avg_spin, ff_avg_break_x, ff_avg_break_z))
+
 multi_model <- glm(percent_strikeout~ ff_avg_speed + ff_avg_spin + ff_avg_break_x +ff_avg_break_z, data=multi_data )
 summary(multi_model)
 
 #multivariate normality
 plot(multi_model, 2) #looks ok
+ks.test(x=multi_model$residuals, y = pnorm, alternative = "two.sided") #not normal
 #homoscedacitiy
 plot(multi_model, 1) #without the outlier looks fine
+gqtest(formula = multi_model, fraction = .2) #p-value = 0.06367 variance fine
 #no multicollinearity
-mydata.rcorr = rcorr(as.matrix(multy_data))
+mydata.rcorr = rcorr(as.matrix(multi_data))
 mydata.coeff = mydata.rcorr$r
 mydata.p = mydata.rcorr$P
 
@@ -56,9 +30,29 @@ mydata.coeff
 mydata.p #got some issues
 
 #RIDGE REGESSION
+#https://www.statology.org/ridge-regression-in-r/
+#make the model
+multi_complete_data <- multi_data[complete.cases(multi_data),]
+y <- multi_complete_data$percent_strikeout
+x <- data.matrix(multi_complete_data[,c('ff_avg_speed', 'ff_avg_spin', 'ff_avg_break_x', 'ff_avg_break_z')])
+ridge_model <- glmnet(x, y, alpha = 0)
+summary(ridge_model)
+
+##perform k-fold cross-validation to find optimal lambda value
+cv_model <- cv.glmnet(x, y, alpha = 0)
+
+#find optimal lambda value that minimizes test MSE
+best_lambda <- cv_model$lambda.min
+best_lambda #1.195782
+
+#produce plot of test MSE by lambda value
+plot(cv_model) 
+
+#look at the best model
+best_model <- glmnet(x, y, alpha = 0, lambda = best_lambda)
+coef(best_model)
+
+#produce Ridge trace plot to visualize how the coefficient estimates changed as a result of increasing lambda
+plot(model, xvar = "lambda")
 
 #LASOO REGRESSION
-
-#WEIGHTED LEAST SQUARES REGRESSION
-
-#K NEAREST NEIGHBORS
