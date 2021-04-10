54ded9418ef25701f4dea433e1bdf9e7ee22d590
Ryan Schultz <ryanschultz2018@yahoo.com>
2021-04-09 14:07:11 -1000

    Clarifying results of tests

--- datacleaning_regression.R
+++ datacleaning_regression.R
@@ -42,15 +42,20 @@ summary(leastSquares)
 #check for homoscedasticity
 plot(leastSquares, 1) #fat in the middle
 gqtest(formula = leastSquares, fraction = .2) # https://www.statisticshowto.com/goldfeld-quandt-test/
+#variance does not significantly change (good)
 
 #Independence: players on different teams are probably independent players on the same team may be trained in similar ways 
+
 #check for autocorrelation
 dwtest(formula = leastSquares) #https://www.statisticshowto.com/durbin-watson-test-coefficient/
 # https://www.itl.nist.gov/div898/handbook/eda/section3/eda35c.htm
+# https://www.statisticssolutions.com/autocorrelation/
+#no autocorrelation (good)
 
 #check for normality
 ggqqplot(leastSquares$residuals)
-ks.test(x=leastSquares$residuals, y = pnorm, alternative = "two.sided")
+ks.test(x=leastSquares$residuals, y = pnorm, alternative = "two.sided") #https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test
+#not normal (bad)
 
 #### Multiple Linear Regression #####
 strikeoutManyVariables <- read.csv(paste0(currentDirectory, "/strikeout_many_variables.csv"))
